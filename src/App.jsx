4de8b21ce4c9da5515068f9f438b866f5ddc6a25
honelgn19 <honelgnmetaferiya@gmail.com>
2026-04-17 15:45:20 +0300

    netflix-start

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import react  from 'react'
+
+import "@fontsource/inter";
+
+function App() {
+  
+
+  return (
+    <>
+      <div><p>Hello, Vite + React! </p></div>
+
+    </>
+  )
+}
+
+export default App
